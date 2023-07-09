43bbf14a5238ff110d4416c98c596d84d269a68c
Xinying Peh <jupitoria@gmail.com>
2023-07-09 11:00:42 +0800

    insert routes and route

--- marketplace-frontend/src/App.js
+++ marketplace-frontend/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { CookiesProvider } from "react-cookie";
+import Register from "./components/RegistrationPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CookiesProvider>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </CookiesProvider>
+    </>
   );
 }
 
