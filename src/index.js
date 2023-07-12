0a4bd3ebbf1fcf1fc822f81e830989777251359f
Xinying Peh <jupitoria@gmail.com>
2023-07-12 20:02:36 +0800

    move router to entry point in index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { CookiesProvider } from "react-cookie";
+import AuthProvider from "./components/auth/AuthProvider";
+import Authenticated from './components/auth/AuthenticatedOnly';
+import Guest from './components/auth/GuestOnly';
+import Root from "./routes/root";
+import Register from "./components/RegistrationPage";
+import Login from "./components/LoginPage";
+import Profile from "./components/ProfilePage";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/users/register",
+    element: <Guest component={Register} />,
+  },
+  {
+    path: "/users/login",
+    element: <Guest component={Login} />,
+  },
+  {
+    path: "/users/profile",
+    element: <Authenticated component={Profile} />,
+  },
+  {
+    path: "/menu-items",
+    element: <Root />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-  <App />
-  </BrowserRouter>
-
+  <React.StrictMode>
+    <CookiesProvider>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </CookiesProvider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
