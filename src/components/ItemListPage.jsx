2ca0c1ce0132bcc74ed19fcc3357320c17559eac
Xinying Peh <jupitoria@gmail.com>
2023-07-16 00:12:14 +0800

    add link to on img of items

--- src/components/ItemListPage.jsx
+++ src/components/ItemListPage.jsx
@@ -41,11 +41,13 @@ export default function ItemListPage() {
               return (
                 <div key={item._id} className="group relative">
                   <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-                  <Link to={`/items/${item._id}`}><img
-                      src={item.image}
-                      alt="thumbnail"
-                      className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-                    /></Link>
+                    <Link to={`/items/${item._id}`}>
+                      <img
+                        src={item.image}
+                        alt="thumbnail"
+                        className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                      />
+                    </Link>
                   </div>
                   <div className="mt-4 flex justify-between">
                     <div>
