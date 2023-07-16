7bd7da9140654adfe6b3cbceec0db47c6564fa21
Xinying Peh <jupitoria@gmail.com>
2023-07-16 23:58:02 +0800

    create edit profile page

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -62,7 +62,6 @@ export default function EditProfile() {
                   name="name"
                   type="name"
                   autoComplete="name"
-                  required
                   className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   onChange={(e) => {
                     handleFormChange(e, "name");
@@ -84,7 +83,6 @@ export default function EditProfile() {
                   name="email"
                   type="email"
                   autoComplete="email"
-                  required
                   className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   onChange={(e) => {
                     handleFormChange(e, "email");
@@ -108,7 +106,6 @@ export default function EditProfile() {
                   name="password"
                   type="password"
                   autoComplete="current-password"
-                  required
                   className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   onChange={(e) => {
                     handleFormChange(e, "password");
