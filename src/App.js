615ee414cdc53697e896f22f50fad7ccc828f18a
Xinying Peh <jupitoria@gmail.com>
2023-07-18 17:01:53 +0800

    change item page from guest only to authenticated

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
         <Route path="/user/profile" element={<Authenticated component={Profile} />} />
         <Route path="/user/update" element={<Authenticated component={EditProfile} />} />
         <Route path="/items" element={<Authenticated component={ItemListPage} />} />
-        <Route path="/items/:itemID" element={<Guest component={ItemPage} />} />
+        <Route path="/items/:itemID" element={<Authenticated component={ItemPage} />} />
         <Route path="/cart" element={<Authenticated component={Cart} />} />
         <Route path="/payment/checkout" element={<Authenticated component={PayButton} />} />
         <Route path="/payment/success" element={<PaymentSuccess />} />
