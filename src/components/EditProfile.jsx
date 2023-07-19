adef41ac03e16850e53fbdcfd0a197fd0efa5854
Xinying Peh <jupitoria@gmail.com>
2023-07-19 23:25:43 +0800

    install validators, add fe validators that shows up on client side

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -2,23 +2,44 @@ import axios from "axios";
 import { useContext, useState } from "react";
 import { AuthContext } from "../components/auth/AuthProvider";
 import { useNavigate } from "react-router-dom";
+import validator from "validator"; // Import the validator library
 
 export default function EditProfile() {
+  // Get the user token from the AuthContext
   const { getUserToken } = useContext(AuthContext);
   const userToken = getUserToken();
+
+  // Navigate to the user's profile
   const navigate = useNavigate();
-  
+
   // create state to store form data
   const [formData, setFormData] = useState({});
 
+  // create state to store form validation errors
+  const [formErrors, setFormErrors] = useState({});
+
+  // Handle form change
   const handleFormChange = (e, fieldName) => {
-    console.log(e.target.value);
     setFormData({ ...formData, [fieldName]: e.target.value });
+    // Clear the corresponding error when the user types
+    setFormErrors({ ...formErrors, [fieldName]: "" });
   };
 
+  // Handle form submit
   const handleSubmit = (e) => {
     e.preventDefault();
 
+    // Perform client-side validation
+    const validationErrors = validateForm(formData);
+
+    // Check for validation errors
+    if (Object.keys(validationErrors).length > 0) {
+      setFormErrors(validationErrors);
+      return;
+    }
+
+    // If no validation errors, continue with the API call
+    // Call the API to update the user
     axios
       .put("http://localhost:3000/api/user/update", formData, {
         headers: {
@@ -32,6 +53,35 @@ export default function EditProfile() {
         console.log(err);
       });
   };
+
+  const validateForm = (data) => {
+    const errors = {};
+  
+    // Validate name field
+    if (data.name && !validator.isLength(data.name, { min: 3, max: 100 })) {
+      errors.name = "Username must be between 3 and 100 characters.";
+    }
+  
+    // Validate email field
+    if (data.email && !validator.isEmail(data.email)) {
+      errors.email = "Invalid email address.";
+    }
+  
+    // Validate password field
+    if (
+      data.password &&
+      !validator.matches(
+        data.password,
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+      )
+    ) {
+      errors.password =
+        "Password must contain at least one lowercase letter, one uppercase letter, one digit, and one special character.";
+    }
+  
+    return errors;
+  };
+  
 
   return (
     <>
@@ -60,13 +110,16 @@ export default function EditProfile() {
                 <input
                   id="name"
                   name="name"
-                  type="name"
+                  type="text" 
                   autoComplete="name"
                   className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   onChange={(e) => {
                     handleFormChange(e, "name");
                   }}
                 />
+                {formErrors.name && (
+                  <p className="text-red-500 text-sm">{formErrors.name}</p>
+                )}
               </div>
             </div>
 
@@ -88,6 +141,9 @@ export default function EditProfile() {
                     handleFormChange(e, "email");
                   }}
                 />
+                {formErrors.email && (
+                  <p className="text-red-500 text-sm">{formErrors.email}</p>
+                )}
               </div>
             </div>
 
@@ -111,6 +167,9 @@ export default function EditProfile() {
                     handleFormChange(e, "password");
                   }}
                 />
+                {formErrors.password && (
+                  <p className="text-red-500 text-sm">{formErrors.password}</p>
+                )}
               </div>
             </div>
 
