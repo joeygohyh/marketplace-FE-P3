1970260d7615197aae79efd0f3f8cf4dc7f4f253
joeygohyh <joeyygoh97@gmail.com>
2023-07-21 12:27:52 +0800

    updated redirect buttons for payment success and failure

--- src/components/PaymentFailed.jsx
+++ src/components/PaymentFailed.jsx
@@ -10,10 +10,10 @@ export default function PaymentFailed() {
               <Link to="/items"
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Go back to shop
+                Back to Shop
               </Link>
-              <a href="#" className="text-sm font-semibold text-gray-900">
-                Contact support <span aria-hidden="true">&rarr;</span>
+              <a href="/contactus" className="text-sm font-semibold text-gray-900">
+                Contact Support <span aria-hidden="true">&rarr;</span>
               </a>
             </div>
           </div>
