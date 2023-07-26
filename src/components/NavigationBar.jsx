b53f799ab380f1fa13f7511f4487072ee4ff26f3
Xinying Peh <jupitoria@gmail.com>
2023-07-26 09:05:00 +0800

    edit contact path name

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -67,7 +67,7 @@ export default function Navigation() {
           >
             Shop
           </Link>
-          <Link to="/ContactUs" className="text-sm font-semibold leading-6 text-gray-900">
+          <Link to="/contactus" className="text-sm font-semibold leading-6 text-gray-900">
           Contact us
           </Link>
 
@@ -158,12 +158,12 @@ export default function Navigation() {
                   Shop
                 </Link>
 
-                {/* <Link
-                  to="/cart"
+                <Link
+                  to="/contactus"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                 >
-                  🛒 Cart
-                </Link> */}
+                  Contact us
+                </Link>
 
                 <Disclosure as="div" className="-mx-3">
                   {({ open }) => (
